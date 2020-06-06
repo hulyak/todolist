4c623d5f71709079090bff9dada0b01341ab87e7
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-05 17:34:22 -0700

    add post route for ejs

--- app.js
+++ app.js
@@ -4,43 +4,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+//set up body parser for post method
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 // set up ejs as a view engine
 app.set('view engine', 'ejs');
 
+//store in the collection
+let items = ["Buy food", "Cook food", "Eat food"];
+// https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
 app.get("/", (req, res) => {
   const today = new Date();
-  const currentDay = today.getDay();
-  var day = "";
-  // day has number value
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to " + currentDay);
-  }
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  const day = today.toLocaleDateString("en-US", options);
+  // render list.ejs passing two variables
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items
   });
+});
+
+//item exists when user types sth
+app.post("/", (req, res) => {
+  const item = req.body.newItem;
+  // console.log(item);
+  //render new list item
+  items.push(item);
+  //triggers get route
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
